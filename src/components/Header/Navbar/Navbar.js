f88a4f9949340526d0dc75029d49315937e5c4e0
Hayk-HM <manvelyanhayk@gmail.com>
2021-07-03 09:35:43 +0400

    Menu for mobile version

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import React from 'react'
 import { useSelector } from 'react-redux'
 import logo from '../../../image/logo.jpg'
 import './Navbar.css'
 import { Link as ScrollLink } from 'react-scroll'
+import { TiThMenu } from 'react-icons/ti'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Navbar = () => {
 
   const header = useSelector(state => state.header)
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
     <div className='header'>
@@ -15,13 +19,14 @@ const Navbar = () => {
           <img src={logo} alt='logo' />
         </div>
         <div className='nav-list'>
-          <ul className='nav-item'>
-            <li><ScrollLink to='testimonials' smooth={true} duration={1000}>Testimonials</ScrollLink></li>
-            <li><ScrollLink to='adventages' smooth={true} duration={1000}>Adventages</ScrollLink></li>
-            <li><ScrollLink to='faq' smooth={true} duration={1000}>FAQ</ScrollLink></li>
-            <li>Sign In</li>
+          <ul className={`nav-item ${isOpen ? "" : 'nav-item-mobile'}`}>
+            <li><ScrollLink onClick={() => setIsOpen(true)} to='testimonials' smooth={true} duration={1000}>Testimonials</ScrollLink></li>
+            <li><ScrollLink onClick={() => setIsOpen(true)} to='adventages' smooth={true} duration={1000}>Adventages</ScrollLink></li>
+            <li><ScrollLink onClick={() => setIsOpen(true)} to='faq' smooth={true} duration={1000}>FAQ</ScrollLink></li>
+            <li onClick={() => setIsOpen(true)}>Sign In</li>
           </ul>
         </div>
+        {isOpen ? <div className='nav-menu' onClick={() => setIsOpen(false)}  ><TiThMenu size={40} /></div> : <div className='nav-closeMenu' onClick={() => setIsOpen(true)}><AiOutlineClose size={40} /></div>}
       </div>
       <div className='sub'>
         <div className='description'>
